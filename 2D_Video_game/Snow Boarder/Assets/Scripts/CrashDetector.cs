ce2298fa73c5dc8c3f0288f729534c0c97613703
Loc Nguyen Tran <phuocloc742@gmail.com>
2024-07-01 17:10:20 +0700

    third commit

---
+++ 2D_Video_game/Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    [SerializeField] float LoadDeplay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+    bool hasCrasher = false;
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Ground" && !hasCrasher)
+        {
+            hasCrasher = true;
+            FindObjectOfType<PlayerController>().DisableControls();
+            crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
+            Invoke("ReloadScene", LoadDeplay);         
+        }    
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
