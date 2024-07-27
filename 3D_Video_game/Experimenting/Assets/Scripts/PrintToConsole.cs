7452f7d0d25f622dfc6b00564cf656e618a866eb
Phuoc Loc Nguyen Tran <phuocloc742@gmail.com>
2024-07-27 18:01:48 +0700

    twenty sixth commit

---
+++ 3D_Video_game/Experimenting/Assets/Scripts/PrintToConsole.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World, First time with Unity 3D - 27/7/2024");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
