641a1721941505d88095eec8b394e8ff74771037
Phuoc Loc Nguyen Tran <phuocloc742@gmail.com>
2024-08-23 22:45:38 +0700

    forty eighth commit

--- 3D_Video_game/Realm Rush/Assets/Scripts/EnemyMover.cs
+++ 3D_Video_game/Realm Rush/Assets/Scripts/EnemyMover.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] float waitTime = 1f;
     void Start()
     {
-        PrintWayPointName();
+        StartCoroutine(FollowPath());
     }
-    void PrintWayPointName()
+    IEnumerator FollowPath()
     {
         foreach(Waypoint waypoint in path)
         {
-            Debug.Log(waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitTime);
         }
     }
 }
