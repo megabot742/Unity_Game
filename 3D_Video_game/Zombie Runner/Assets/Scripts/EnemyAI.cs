235a1eabf7392397812ac5ba93074292ab48c87b
Phuoc Loc Nguyen Tran <phuocloc742@gmail.com>
2024-09-02 22:11:06 +0700

    fifty sixth commit

--- 3D_Video_game/Zombie Runner/Assets/Scripts/EnemyAI.cs
+++ 3D_Video_game/Zombie Runner/Assets/Scripts/EnemyAI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +7,53 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 8f;
     NavMeshAgent navMeshAgent;
-    // Start is called before the first frame update
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
-
-    // Update is called once per frame
     void Update()
     {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if(isProvoked == true)
+        {
+            EngageTarget();
+        }
+        else if(distanceToTarget <= chaseRange)
+        {   
+            isProvoked = true;
+        }
+    }
+    void EngageTarget()
+    {
+        if(distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        if(distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+    void ChaseTarget()
+    {
         navMeshAgent.SetDestination(target.position);
+        //if player too far, enemy don't chase
+        // if(distanceToTarget > chaseRange + 10f) 
+        // {
+        //     isProvoked = false;
+        // }
+    }
+    void AttackTarget()
+    {
+        Debug.Log("You dead!!!");
+    }
+    void OnDrawGizmosSelected() //Draw enemy chaseRange radius
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);    
     }
 }
